5b9c77cadc71257e66d7a93a004e0492cf5efd01
Andrew Cole <andrew.cole@illallangi.com>
2014-06-15 10:42:04 +1000

    Updated build config for AppVeyor use

--- build.cmd
+++ build.cmd
@@ -3,7 +3,5 @@ set config=%1
 if "%config%" == "" (
    set config=Debug
 )
-
-%WINDIR%\Microsoft.NET\Framework\v4.0.30319\msbuild build.proj /p:Configuration="%config%" /m /v:M /fl /flp:LogFile=msbuild.log;Verbosity=Normal /nr:false
-
+%WINDIR%\Microsoft.NET\Framework\v4.0.30319\msbuild "%~dp0src\build.proj"
 pause
